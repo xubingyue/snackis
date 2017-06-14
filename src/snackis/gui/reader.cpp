1e9c354fd85d61a77e849fef0b11ca1658910d30
Andreas <andreas.gone.wild@gmail.com>
2017-06-14 12:06:26 +0200

    added invite command

--- src/snackis/gui/reader.cpp
+++ src/snackis/gui/reader.cpp
@@ -1,4 +1,5 @@
 #include "snackis/ctx.hpp"
+#include "snackis/invite.hpp"
 #include "snackis/core/fmt.hpp"
 #include "snackis/gui/gui.hpp"
 #include "snackis/gui/reader.hpp"
@@ -6,25 +7,64 @@
 
 namespace snackis {
 namespace gui {
-  static void on_activate(GtkWidget *_, Reader *rdr) {
-    str in(gtk_entry_get_text(GTK_ENTRY(rdr->entry)));
+  static bool exec_cmd(Reader &rdr, const str &in) {
+    Ctx &ctx(rdr.ctx);
+    str in_str(in);
     
-    if (in == "") {
-      if (!rdr->last_cmd) { return; }
-      in = *rdr->last_cmd;
+    if (in_str == "") {
+      if (!rdr.last_cmd) { return false; }
+      in_str = *rdr.last_cmd;
     }
+
+    Stream in_words(in_str);
+    str cmd;
+    in_words >> cmd;
+    std::vector<str> args;
+    str arg;
+    while (in_words >> arg) { args.push_back(arg); }
     
-    if (in == "quit") { quit(); }
-    else if (in == "setup") {
-      Setup *setup = new Setup(rdr->ctx);
+    if (cmd == "invite") {
+      if (args.size() != 1) {
+	log(ctx, "Invalid number of arguments, syntax: invite foo@bar.com");
+	return false;
+      }
+
+      db::Trans trans(ctx);
+      Invite inv(ctx, args[0]);
+      load(ctx.db.invites, inv);
+      post_msg(inv);	
+      log(ctx, fmt("New invite saved to outbox: %0", inv.to));
+      db::commit(trans);
+    } else if (cmd == "quit") {
+      if (!args.empty()) {
+	log(ctx, "Invalid number of arguments, syntax: quit");
+	return false;
+      }
+      
+      quit();
+    }
+    else if (cmd == "setup") {
+      if (!args.empty()) {
+	log(ctx, "Invalid number of arguments, syntax: setup");
+	return false;
+      }
+
+      Setup *setup = new Setup(ctx);
       setup->push_view();
     } else {
-      log(rdr->ctx, fmt("Unknown command: '%0'", in));
-      return;
+      log(ctx, fmt("Unknown command: '%0'", in_str));
+      return false;
     }
     
-    rdr->last_cmd = in;
-    gtk_entry_set_text(GTK_ENTRY(rdr->entry), "");
+    rdr.last_cmd = in_str;
+    return true;
+  }
+
+
+  static void on_activate(GtkWidget *_, Reader *rdr) {
+    if (exec_cmd(*rdr, gtk_entry_get_text(GTK_ENTRY(rdr->entry)))) {
+      gtk_entry_set_text(GTK_ENTRY(rdr->entry), "");
+    }
   }
   
   Reader::Reader(Ctx &ctx): ctx(ctx), entry(gtk_entry_new()) {
