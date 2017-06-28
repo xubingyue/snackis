bc859e5c40729b934792e222c296d66fe25129a6
Andreas <andreas.gone.wild@gmail.com>
2017-06-29 00:49:34 +0200

    new dist

--- src/snackis/snackis.cpp
+++ src/snackis/snackis.cpp
@@ -6,7 +6,7 @@
 #include "snackis/crypt/error.hpp"
 
 namespace snackis {
-  const int VERSION[3] = {0, 7, 1};
+  const int VERSION[3] = {0, 7, 2};
   const int64_t DB_REV = 2;
   const int64_t PROTO_REV = 2;
   
