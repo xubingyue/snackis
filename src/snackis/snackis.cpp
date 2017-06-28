bcf511faaf4fdfb5caab95aaded09e4b5935d81f
Andreas <andreas.gone.wild@gmail.com>
2017-06-29 00:50:42 +0200

    new dist

--- src/snackis/snackis.cpp
+++ src/snackis/snackis.cpp
@@ -6,9 +6,9 @@
 #include "snackis/crypt/error.hpp"
 
 namespace snackis {
-  const int VERSION[3] = {0, 7, 2};
+  const int VERSION[3] = {0, 7, 3};
   const int64_t DB_REV = 2;
-  const int64_t PROTO_REV = 2;
+  const int64_t PROTO_REV = 3;
   
   void init() {
     if (sodium_init() == -1) {
