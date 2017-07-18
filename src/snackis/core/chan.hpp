22d4d496ca4e8190c1a0199e3a00ad34ddfbccfd
Andreas <andreas.gone.wild@gmail.com>
2017-07-18 16:51:49 +0200

    opt chan conds

--- src/snackis/core/chan.hpp
+++ src/snackis/core/chan.hpp
@@ -10,7 +10,7 @@ namespace snackis {
     const size_t max;
     std::deque<T> buf;
     std::mutex mutex;
-    std::condition_variable get_ok, put_ok;
+    opt<std::condition_variable> get_ok, put_ok;
     bool closed;
 
     Chan(size_t max);
@@ -28,7 +28,7 @@ namespace snackis {
     ChanLock lock(c.mutex);
     CHECK(c.closed, !_);
     c.closed = true;
-    c.put_ok.notify_all();
+    if (c.put_ok) { c.put_ok->notify_all(); }
   }
 
   template <typename T>
@@ -36,22 +36,25 @@ namespace snackis {
     ChanLock lock(c.mutex);
 
     while (!c.buf.empty()) {
-      c.get_ok.notify_all();
-      c.put_ok.wait(lock, [&c](){ return c.buf.empty(); });
+      if (c.get_ok) { c.get_ok->notify_all(); }
+      if (!c.put_ok) { c.put_ok.emplace(); }
+      c.put_ok->wait(lock, [&c](){ return c.buf.empty(); });
     }
   }
 
   template <typename T>
   bool put(Chan<T> &c, const T &it, bool wait=true) {
     ChanLock lock(c.mutex);
+    if (c.closed) { return false; }
 
     if (wait && c.buf.size() == c.max) {
-      c.put_ok.wait(lock, [&c](){ return c.closed || c.buf.size() < c.max; });
+      if (!c.put_ok) { c.put_ok.emplace(); }
+      c.put_ok->wait(lock, [&c](){ return c.closed || c.buf.size() < c.max; });
     }
 
     if (c.buf.size() == c.max) { return false; }
     c.buf.push_back(it);
-    c.get_ok.notify_one();
+    if (c.get_ok) { c.get_ok->notify_one(); }
     return true;
   }
 
@@ -60,13 +63,14 @@ namespace snackis {
     ChanLock lock(c.mutex);
     
     if (wait && c.buf.empty()) {
-      c.get_ok.wait(lock, [&c](){ return c.closed || !c.buf.empty(); });
+      if (!c.get_ok) { c.get_ok.emplace(); }
+      c.get_ok->wait(lock, [&c](){ return c.closed || !c.buf.empty(); });
     }
     
     if (c.buf.empty()) { return nullopt; }
     auto out(c.buf.front());
     c.buf.pop_front();
-    c.put_ok.notify_one();
+    if (c.put_ok) { c.put_ok->notify_one(); }
     return out;
   }
 }
