48146baf746e1fa83206bc2a56317c9009b07cdb
Andreas <andreas.gone.wild@gmail.com>
2017-07-23 23:45:27 +0200

    simplified chan

--- src/snackis/core/chan.hpp
+++ src/snackis/core/chan.hpp
@@ -10,7 +10,7 @@ namespace snackis {
     const size_t max;
     std::deque<T> buf;
     std::mutex mutex;
-    opt<std::condition_variable> get_ok, put_ok;
+    std::condition_variable get_ok, put_ok;
     bool closed;
 
     Chan(size_t max);
@@ -28,8 +28,8 @@ namespace snackis {
     ChanLock lock(c.mutex);
     CHECK(c.closed, !_);
     c.closed = true;
-    if (c.get_ok) { c.get_ok->notify_all(); }
-    if (c.put_ok) { c.put_ok->notify_all(); }
+    c.get_ok.notify_all();
+    c.put_ok.notify_all();
   }
 
   template <typename T>
@@ -38,13 +38,12 @@ namespace snackis {
     if (c.closed) { return false; }
 
     if (wait && c.buf.size() == c.max) {
-      if (!c.put_ok) { c.put_ok.emplace(); }
-      c.put_ok->wait(lock, [&c](){ return c.closed || c.buf.size() < c.max; });
+      c.put_ok.wait(lock, [&c](){ return c.closed || c.buf.size() < c.max; });
     }
 
     if (c.buf.size() == c.max) { return false; }
     c.buf.push_back(it);
-    if (c.get_ok) { c.get_ok->notify_one(); }
+    c.get_ok.notify_one();
     return true;
   }
 
@@ -53,14 +52,13 @@ namespace snackis {
     ChanLock lock(c.mutex);
     
     if (wait && c.buf.empty()) {
-      if (!c.get_ok) { c.get_ok.emplace(); }
-      c.get_ok->wait(lock, [&c](){ return c.closed || !c.buf.empty(); });
+      c.get_ok.wait(lock, [&c](){ return c.closed || !c.buf.empty(); });
     }
     
     if (c.buf.empty()) { return nullopt; }
     auto out(c.buf.front());
     c.buf.pop_front();
-    if (c.put_ok) { c.put_ok->notify_one(); }
+    c.put_ok.notify_one();
     return out;
   }
 }
