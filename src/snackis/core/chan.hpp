d8fd43e5c38fff6041445d0caa72fd12d2e3b4bd
Andreas <andreas.gone.wild@gmail.com>
2017-07-24 07:47:50 +0200

    chan perf

--- src/snackis/core/chan.hpp
+++ src/snackis/core/chan.hpp
@@ -2,6 +2,7 @@
 #define SNACKIS_CHAN_HPP
 
 #include <atomic>
+#include <condition_variable>
 #include <mutex>
 #include <vector>
 #include "snackis/core/error.hpp"
@@ -14,13 +15,16 @@ namespace snackis {
     size_t pos;
     std::atomic<size_t> size;
     std::mutex mutex;
-    std::atomic<bool> closed;
+    std::condition_variable get_ok, put_ok;
+    bool closed;
 
     Chan(size_t max);
   };
 
   using ChanLock = std::unique_lock<std::mutex>;
   
+  const int CHAN_RETRIES(10);
+
   template <typename T>
   Chan<T>::Chan(size_t max):
     max(max), pos(0), size(0), closed(false)
@@ -31,53 +35,68 @@ namespace snackis {
   template <typename T>
   void close(Chan<T> &c) {    
     ChanLock lock(c.mutex);
-    CHECK(c.closed.load(), !_);
-    c.closed.store(true);
+    CHECK(c.closed, !_);
+    c.closed = true;
+    c.get_ok.notify_all();
+    c.put_ok.notify_all();
   }
 
   template <typename T>
   bool put(Chan<T> &c, const T &it, bool wait=true) {
-    while (true) {
-      if (c.size.load() == c.max) {
-	if (!wait || c.closed.load()) { break; }
+    if (c.size.load() == c.max) {
+      if (!wait) { return false; }
+      int i(0);
+
+      do { 
 	std::this_thread::yield();
-	continue;
-      }
+	i++;
+      } while (c.size.load() == c.max && i <= CHAN_RETRIES);
+    }
     
-      ChanLock lock(c.mutex);
-      if (c.buf.size() == c.max) { continue; }
-      c.buf.push_back(it);
-      c.size++;
-      return true;
+    ChanLock lock(c.mutex);
+    if (c.closed) { return false; }
+
+    if (wait && c.buf.size() == c.max) {
+      c.put_ok.wait(lock, [&c](){ return c.closed || c.buf.size() < c.max; });
     }
 
-    return false;
+    if (c.buf.size() == c.max) { return false; }
+    c.buf.push_back(it);
+    c.size++;
+    c.get_ok.notify_one();
+    return true;
   }
 
   template <typename T>
   opt<T> get(Chan<T> &c, bool wait=true) {
-    while (true) {
-      if (c.size.load() == 0) {
-	if (!wait || c.closed.load()) { break; }
-	std::this_thread::yield();
-	continue;
-      }
+    if (c.size.load() == 0) {
+      if (!wait) { return nullopt; }
+      int i(0);
 
-      ChanLock lock(c.mutex);
-      if (c.pos == c.buf.size()) { continue; }
-      auto out(c.buf[c.pos]);
-      c.pos++;
-      
-      if (c.pos == c.buf.size()) {
-	c.buf.clear();
-	c.pos = 0;
-      }
-      
-      c.size--;
-      return out;
+      do { 
+	std::this_thread::yield(); 
+	i++;
+      } while (c.size.load() == 0 && i <= CHAN_RETRIES);
     }
 
-    return nullopt;
+    ChanLock lock(c.mutex);
+    
+    if (wait && c.pos == c.buf.size()) {
+      c.get_ok.wait(lock, [&c](){ return c.closed || c.pos < c.buf.size(); });
+    }
+    
+    if (c.pos == c.buf.size()) { return nullopt; }
+    auto out(c.buf[c.pos]);
+    c.pos++;
+
+    if (c.pos == c.buf.size()) {
+      c.buf.clear();
+      c.pos = 0;
+    }
+    
+    c.size--;
+    c.put_ok.notify_one();
+    return out;
   }
 }
 
