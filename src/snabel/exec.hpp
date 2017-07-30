bba3bf10b1e5bff0fed27be622aa0b45e1239f46
Andreas <andreas.gone.wild@gmail.com>
2017-07-30 17:59:02 +0200

    use fiber

--- src/snabel/exec.hpp
+++ src/snabel/exec.hpp
@@ -3,19 +3,19 @@
 
 #include <map>
 
-#include "snabel/coro.hpp"
+#include "snabel/fiber.hpp"
 
 namespace snabel {  
   struct Exec {
-    std::map<size_t, Coro> coros;
-    Coro &main;
+    std::map<size_t, Fiber> fibers;
+    Fiber &main;
 
     Type &i64_type, &str_type;
     
     Exec():
-      main(coros.emplace(std::piecewise_construct,
-			 std::forward_as_tuple(0),
-			 std::forward_as_tuple(*this, 0)).first->second),
+      main(fibers.emplace(std::piecewise_construct,
+			  std::forward_as_tuple(0),
+			  std::forward_as_tuple(*this, 0)).first->second),
       i64_type((add_type(main.root_ctx, "i64"))),
       str_type((add_type(main.root_ctx, "str")))
     { }
--- src/snabel/fiber.hpp
+++ src/snabel/fiber.hpp
@@ -1,14 +1,13 @@
 #ifndef SNABEL_FIBER_HPP
 #define SNABEL_FIBER_HPP
 
-#include <vector>
+#include "snabel/coro.hpp"
 
 namespace snabel {
-  struct Fiber {
-    std::vector<Op> ops;
-    size_t pc;
-
-    Fiber();
+  struct Fiber: Coro {
+    Fiber(Exec &exe, int64_t id):
+      Coro(exe, id)
+    { }
   };
 }
 
